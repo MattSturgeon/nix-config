e8f92dbce8c7781e1cc28665c5eda269bf3a8703
Matt Sturgeon <matt@sturgeon.me.uk>
2023-06-06 18:35:07 +0100

    Initial commit
    Based on [Misterio's starter config
    example](https://github.com/Misterio77/nix-starter-configs).

---
+++ README.md
@@ -0,0 +1,15 @@
+# My nix configuration
+
+This is a nix flake defining my system and user configurations. It is currently work in progress and very incomplete.
+
+## Initial bootstrap
+
+If the experimental feature `nix-command` and `flakes` not enabled, run `nix-shell` from this directory to enable them.
+
+Run `nixos-rebuild --flake .#matts-laptop` to install the system configuration.
+
+Run `home-manager --flake .#matt@matts-laptop` to install the user configuration.
+
+## Updating
+
+TODO
---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "My nix config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+    let
+      inherit (self) outputs;
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+    in
+    rec {
+      # Devshell for bootstrapping
+      # Acessible through `nix develop` or `nix-shell` (legacy)
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in import ./shell.nix { inherit pkgs; }
+      );
+
+      # TODO Add any overlays/modules here?
+
+      # NixOS configuration entrypoint
+      # Available through `nixos-rebuild --flake .#matts-laptop`
+      nixosConfigurations = {
+        matts-laptop = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            # Main nixos configuration file
+            ./nixos/configuration.nix
+          ];
+        };
+      };
+
+      # Standalone home-manager configuration entrypoint
+      # Available through `home-manager --flake .#matt@matts-laptop`
+      homeConfigurations = {
+        "matt@matts-laptop" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [
+            # Main home-manager configuration file
+            ./home-manager/home.nix
+          ];
+        };
+      };
+    };
+}
