bd11807ac196402098d98d79080adb28b325caad
Matt Sturgeon <matt@sturgeon.me.uk>
2023-06-13 21:24:19 +0100

    Force installing firefox search config

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -8,7 +8,10 @@
 	id = 0;
 	name = "Matt Sturgeon";
 	isDefault = true;
-	search.default = "google"; # TODO Move to something more privacy respecting?
+	search = {
+	  default = "google"; # TODO Move to something more privacy respecting?
+	  force = true; # Firefox often replaces the symlink, so force on update
+	};
         # extensions = [ ]; # (some are packaged in NUR)
 	# extraConfig = '' ''; # user.js
 	# userChrome = '' ''; # chrome CSS
