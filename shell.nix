d3f5175f7dfec4bef15ee1a10027ae6be8fbe5e9
Matt Sturgeon <matt@sturgeon.me.uk>
2023-06-26 18:49:36 +0100

    fix(shell.nix): fix bootstrapping shell
    Fix a broken reference to `./nixpkgs.nix`, which I don't use.

--- shell.nix
+++ shell.nix
@@ -1,7 +1,17 @@
 # Shell for bootstrapping flake-enabled nix and home-manager
 # You can enter it through 'nix develop' or (legacy) 'nix-shell'
 
-{ pkgs ? (import ./nixpkgs.nix) { } }: {
+{ pkgs ? # If pkgs is not defined, instanciate nixpkgs from locked commit
+  let
+    lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
+    nixpkgs = fetchTarball {
+      url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
+      sha256 = lock.narHash;
+    };
+  in
+  import nixpkgs { overlays = [ ]; }
+, ...
+}: {
   default = pkgs.mkShell {
     # Enable experimental features without having to specify the argument
     NIX_CONFIG = "experimental-features = nix-command flakes";
