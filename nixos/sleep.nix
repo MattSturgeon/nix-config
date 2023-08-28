7c02f65f61874c873f84c6fb38501f63d2ae5226
Matt Sturgeon <matt@sturgeon.me.uk>
2023-08-28 17:33:10 +0100

    feat(sleep): use sleep-to-idle instead of s3
    s2idle and s3 should have approximately the same power usage, but the
    former will be a little faster.

--- nixos/sleep.nix
+++ nixos/sleep.nix
@@ -4,13 +4,13 @@
 
     # When using sleep-then-hibernate,
     # sleep for 2h before hibernating
-    systemd.sleep.extraConfig = "HibernateDelaySec=30s";
+    systemd.sleep.extraConfig = "HibernateDelaySec=2h";
 
     # s2idle or deep may work better depending on hardware...
     # "deep" is traditional S3 sleep (suspend to RAM)
     # "s2idle" is a more modern low power sleep, aka S0ix.
     #     This _should_ use similar power but be faster.
-    boot.kernelParams = [ "mem_sleep_default=deep" ];
+    boot.kernelParams = [ "mem_sleep_default=s2idle" ];
 
     # FIXME this shouldn't be needed...
     # when empty, it should default to the first entry in swapDevices
