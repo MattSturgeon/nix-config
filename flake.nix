2f8e87eb00ec1f1885536d49beefaee1332fa509
Matt Sturgeon <matt@sturgeon.me.uk>
2023-10-10 14:36:35 +0100

    fixup! make home manager use nixpkgs unstable

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     hardware.url = "github:nixos/nixos-hardware";
