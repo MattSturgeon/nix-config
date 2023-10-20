535382eb0a941b58be12bd85dab608b7833b9f7c
Matt Sturgeon <matt@sturgeon.me.uk>
2023-10-20 21:05:04 +0100

    Cleanup: remove custom pkgs & overlays

--- overlays/default.nix
+++
@@ -1,9 +0,0 @@
-# This file defines overlays
-# https://nixos.wiki/wiki/Overlays
-{...}: {
-  # Overlay custom packages into pkgs
-  additions = final: prev:
-    import ../pkgs {
-      pkgs = prev;
-    };
-}
