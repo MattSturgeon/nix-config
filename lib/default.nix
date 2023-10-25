db3c41079244e7fece94b24f0022ba05a5780239
Matt Sturgeon <matt@sturgeon.me.uk>
2023-10-25 16:07:53 +0100

    Fix libs not being merged recursively
    `mergeAttrsList` will clobber nested attr sets, as it only updates
    top-level sets using the `//` operator.
    
    Instead, we want to update using `recursiveUpdate`, so implement our own
    `recursiveMergeAttrsList` function.

--- lib/default.nix
+++ lib/default.nix
@@ -5,8 +5,7 @@
 } @ attrs: let
   lib = inputs.nixpkgs.lib;
   inherit (builtins) filter pathExists dirOf readDir;
-  inherit (lib) mapAttrsToList filterAttrs hasSuffix;
-  inherit (lib.attrsets) mergeAttrsList; # no short form
+  inherit (lib) mapAttrsToList filterAttrs hasSuffix foldr recursiveUpdate;
 
   # Map a list of paths to a list of (imported) nix expressions
   importAll = list: map import list;
@@ -41,6 +40,9 @@
   # including 'default.nix' files found in sibling directories.
   nixSiblings = file: filter (path: path != file) (nixChildren (dirOf file));
 
+  # As per mergeAttrsList, but merge sets recursively
+  recursiveMergeAttrsList = foldr (l: r: recursiveUpdate l r) {};
+
   # Arguments to pass to each imported sibling
   args =
     {
@@ -50,10 +52,10 @@
     // attrs;
 in
   # Merge everything in lib/, including functions defined above
-  mergeAttrsList (
+  recursiveMergeAttrsList (
     [
       {
-        util = {inherit nixChildren nixSiblings importAll;};
+        util = {inherit nixChildren nixSiblings importAll recursiveMergeAttrsList;};
       }
     ]
     ++ (map (f: f args) (importAll (nixSiblings ./default.nix)))
