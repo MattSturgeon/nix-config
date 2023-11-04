dcd7705af76ce9d87eba7b2a480a0aabfa31938c
Matt Sturgeon <matt@sturgeon.me.uk>
2023-11-04 23:52:08 +0000

    Mark lua strings for treesitter

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -155,11 +155,11 @@ in {
           nvim-cmp = {
             enable = true;
             mapping = {
-              "<CR>" = "cmp.mapping.confirm({ select = true })";
-              "C-y" = "cmp.mapping.confirm({ select = true })";
+              "<CR>" = /* lua */ "cmp.mapping.confirm({ select = true })";
+              "C-y" = /* lua */ "cmp.mapping.confirm({ select = true })";
               "<Tab>" = {
                 modes = ["i" "s"];
-                action = ''
+                action = /* lua */ ''
                   function(fallback)
                     if cmp.visible() then
                       cmp.select_next_item()
@@ -177,16 +177,16 @@ in {
               };
               "<S-Tab>" = {
                 modes = ["i" "s"];
-                action = ''
-                              function(fallback)
-                  if cmp.visible() then
-                                  cmp.select_prev_item()
-                                elseif luasnip.jumpable(-1) then
-                                  luasnip.jump(-1)
-                                else
-                                  fallback()
-                                end
-                              end
+                action = /* lua */ ''
+	          function(fallback)
+                    if cmp.visible() then
+                      cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                      luasnip.jump(-1)
+                    else
+                      fallback()
+                    end
+                  end
                 '';
               };
             };
@@ -270,17 +270,17 @@ in {
           vim-be-good
         ];
 
-        extraConfigLuaPre = ''
-                 -- Define some variables used in nvim-cmp keybinds
+        extraConfigLuaPre = /* lua */ ''
+	  -- Define some variables used in nvim-cmp keybinds
           -- as per the example on nvim-cmp's wiki
           local has_words_before = function()
-                   unpack = unpack or table.unpack
-                   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-                   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-                 end
+              unpack = unpack or table.unpack
+              local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+              return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+            end
 
-                 local luasnip = require("luasnip")
-                 local cmp = require("cmp")
+            local luasnip = require("luasnip")
+            local cmp = require("cmp")
         '';
       };
     };
