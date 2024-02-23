29c3a7378c814da75828be32dd73db5057ae4338
Matt Sturgeon <matt@sturgeon.me.uk>
2024-02-23 22:01:58 +0000

    feat(sync): add syncthing
    Keep documents in sync. Potentially notes too?

---
+++ modules/home-manager/sync.nix
@@ -0,0 +1,24 @@
+{
+  pkgs,
+  lib,
+  config,
+  options,
+  ...
+}: let
+  inherit (lib) types mkIf mkOption;
+  cfg = config.custom.sync;
+in {
+  options.custom.sync = {
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable Syncthing syncing";
+    };
+  };
+  config = mkIf cfg.enable {
+    services.syncthing = {
+      enable = true;
+      extraOptions = ["--no-default-folder"];
+    };
+  };
+}
