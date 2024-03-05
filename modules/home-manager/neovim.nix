531d48d8a73a85eea9f4fa4047e2f916990aa2f1
Matt Sturgeon <matt@sturgeon.me.uk>
2024-03-05 12:45:38 +0000

    feat(nvim): Change window motions
    Rather than doing `Ctrl`+Key, map `<leader>w` to `<C-w>`.
    
    Unfortunately this is affected by https://github.com/folke/which-key.nvim/issues/583

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -76,30 +76,12 @@ in {
             options.silent = true;
           }
 
-          # Better window motions
+          # Window motions
           {
             mode = "n";
-            key = "<C-h>";
-            action = "<C-w>h";
-            options.desc = "Go to left window";
-          }
-          {
-            mode = "n";
-            key = "<C-j>";
-            action = "<C-w>j";
-            options.desc = "Go to lower window";
-          }
-          {
-            mode = "n";
-            key = "<C-k>";
-            action = "<C-w>k";
-            options.desc = "Go to upper window";
-          }
-          {
-            mode = "n";
-            key = "<C-l>";
-            action = "<C-w>l";
-            options.desc = "Go to right window";
+            key = "<leader>w";
+            action = "<C-w>";
+            options.desc = "+windows";
           }
 
           # Buffer motions
