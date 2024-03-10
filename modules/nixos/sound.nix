aca3ab8ae717e63411286e3bbbe58dc37ef2f516
Matt Sturgeon <matt@sturgeon.me.uk>
2024-03-10 21:59:21 +0000

    chore(audio): Use new wireplumber `extraConfig`
    Added by https://github.com/NixOS/nixpkgs/pull/292115

--- modules/nixos/sound.nix
+++ modules/nixos/sound.nix
@@ -1,6 +1,5 @@
 { config
 , lib
-, pkgs
 , ...
 }:
 with lib;
@@ -25,16 +24,14 @@ in
       alsa.support32Bit = true;
       pulse.enable = true;
       jack.enable = cfg.jack;
-      wireplumber.configPackages = [
-        (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" /* lua */ ''
-          bluez_monitor.properties = {
-              ["bluez5.enable-sbc-xq"] = true,
-              ["bluez5.enable-msbc"] = true,
-              ["bluez5.enable-hw-volume"] = true,
-              ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-          }
-        '')
-      ];
+      wireplumber.extraConfig = {
+        "monitor.bluez.properties" = {
+            "bluez5.enable-sbc-xq" = true;
+            "bluez5.enable-msbc" = true;
+            "bluez5.enable-hw-volume" = true;
+            "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+        };
+      };
     };
   };
 }
