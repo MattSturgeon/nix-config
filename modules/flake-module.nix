3b813fdb19deaa92b97d21ba28745f52b710ff45
Matt Sturgeon <matt@sturgeon.me.uk>
2024-03-27 17:03:30 +0000

    remove(lib): `util.nix`
    Made redundant by flake-parts `perSystem` and newly added local
    functions in `modules/flake-module.nix`.

--- modules/flake-module.nix
+++ modules/flake-module.nix
@@ -1,25 +1,53 @@
-{ self, ... }:
+{ lib, ... }:
 let
-  inherit (self.lib.util) importChildren;
+  /**
+    List the children of `dir`.
+
+    # Inputs
+
+    `dir`
+
+    : Directory to read
+
+    # Type
+
+    ```
+    children :: path -> [path]
+    ```
+  */
+  children = dir: with builtins; lib.pipe dir [
+    readDir
+    attrNames
+    (map (name: dir + "/${name}"))
+  ];
+
+  /**
+    Wrap all files in `dir` as submodule imports.
+
+    # Inputs
+
+    `dir`
+
+    : Directory to package as a module
+
+    # Type
+
+    ```
+    wrap :: path -> AttrSet
+    ```
+  */
+  wrap = dir: { imports = children dir; };
 in
 {
   flake = {
     nixosModules = {
-      common = {
-        imports = importChildren ../modules/common;
-      };
-      nixos = {
-        imports = importChildren ../modules/nixos;
-      };
+      common = wrap ./common;
+      nixos = wrap ./nixos;
     };
 
     homeModules = {
-      common = {
-        imports = importChildren ../modules/common;
-      };
-      home = {
-        imports = importChildren ../modules/home-manager;
-      };
+      common = wrap ./common;
+      home = wrap ./home-manager;
     };
   };
 }
