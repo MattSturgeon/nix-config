f9128f8cfdf19385878f7b9b52af5662f364a52e
Matt Sturgeon <matt@sturgeon.me.uk>
2024-04-07 04:08:27 +0100

    nix fmt

--- nvim/flake-module.nix
+++ nvim/flake-module.nix
@@ -4,20 +4,22 @@
     nixvimModules.default = import ./config;
   };
 
-  perSystem = { self', pkgs, system, ... }: let
-    inherit (inputs.nixvim.legacyPackages.${system}) makeNixvimWithModule;
-    inherit (inputs.nixvim.lib.${system}.check) mkTestDerivationFromNvim;
-  in {
-    # Run using `nix run .#nvim`
-    packages.nvim = makeNixvimWithModule {
-      inherit pkgs;
-      module = self.nixvimModules.default;
-    };
+  perSystem = { self', pkgs, system, ... }:
+    let
+      inherit (inputs.nixvim.legacyPackages.${system}) makeNixvimWithModule;
+      inherit (inputs.nixvim.lib.${system}.check) mkTestDerivationFromNvim;
+    in
+    {
+      # Run using `nix run .#nvim`
+      packages.nvim = makeNixvimWithModule {
+        inherit pkgs;
+        module = self.nixvimModules.default;
+      };
 
-    # `nix flake check` will also validate config
-    checks.nvim = mkTestDerivationFromNvim {
-      inherit (self'.packages) nvim;
-      name = "My custom neovim";
+      # `nix flake check` will also validate config
+      checks.nvim = mkTestDerivationFromNvim {
+        inherit (self'.packages) nvim;
+        name = "My custom neovim";
+      };
     };
-  };
 }
