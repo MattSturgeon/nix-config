d560f74d5c5809027ed69659526f4e550de3236c
Matt Sturgeon <matt@sturgeon.me.uk>
2024-08-21 07:36:04 +0100

    nixos: enable appimage

---
+++ modules/nixos/appimage.nix
@@ -0,0 +1,16 @@
+{ config, lib, ... }:
+let
+  cfg = config.custom.appimage;
+in
+{
+  options.custom.appimage = {
+    enable = lib.mkEnableOption "appimage" // { default = true; };
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.appimage = {
+      enable = true;
+      binfmt = true;
+    };
+  };
+}
