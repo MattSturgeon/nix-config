fa15d7cac2d6d345168ce45b812f6e976b1513df
Matt Sturgeon <matt@sturgeon.me.uk>
2024-08-28 02:53:37 +0100

    hosts/desktop: add steam & prism to gnome favourites

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -3,7 +3,9 @@
     editors.vscode = true;
     editors.idea = true;
     gnome.favorites = lib.mkAfter [
+      "steam.desktop"
       "com.heroicgameslauncher.hgl.desktop"
+      "org.prismlauncher.PrismLauncher.desktop"
       "idea-community.desktop"
     ];
   };
