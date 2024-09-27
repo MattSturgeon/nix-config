9586bcb98444155ed31c024db76cbd90ce6bf72a
Matt Sturgeon <matt@sturgeon.me.uk>
2024-09-27 04:37:52 +0100

    nvim/which-key: register additional window binds
    which-key only knows about registered keymaps and user-created keymaps.
    Builtin binds without a remap aren't known to which-key unless
    registered.
    
    See https://github.com/folke/which-key.nvim/issues/583

--- nvim/config/keymaps.nix
+++ nvim/config/keymaps.nix
@@ -9,13 +9,36 @@
           group = "windows";
         }
         {
+          __unkeyed-1 = "<c-w>c";
+          desc = "Close current window";
+        }
+        {
+          __unkeyed-1 = "<c-w>H";
+          desc = "Move current window to the far left";
+        }
+        {
+          __unkeyed-1 = "<c-w>J";
+          desc = "Move current window to the very bottom";
+        }
+        {
+          __unkeyed-1 = "<c-w>K";
+          desc = "Move current window to the very top";
+        }
+        {
+          __unkeyed-1 = "<c-w>L";
+          desc = "Move current window to the far right";
+        }
+
+        {
           __unkeyed-1 = "<leader>b";
           group = "buffers";
         }
+
         {
           __unkeyed-1 = "<leader>r";
           group = "refactoring";
         }
+
         {
           __unkeyed-1 = "<leader>f";
           group = "files";
