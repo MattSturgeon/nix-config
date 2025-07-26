f03d45829ea8b0b471a8cfa4597e8014f6525edc
Matt Sturgeon <matt@sturgeon.me.uk>
2025-07-26 16:25:56 +0100

    refactor(nix): use `nix.nixPath` option
    This option is now on home-manager too.

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -24,12 +24,10 @@ in
       # This will add each flake input as a registry
       # To make nix3 commands consistent with your flake
       registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+      # Add flake registries to legacy channels, making legacy nix commands consistent
+      nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
 
       settings = {
-        # Set nix-path in nix.settings because nix.nixPath isn't supported on home-manager
-        # Add flake registries to legacy channels, making legacy nix commands consistent
-        nix-path = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
-
         # Enable flakes and new 'nix' command
         experimental-features = "nix-command flakes";
 
