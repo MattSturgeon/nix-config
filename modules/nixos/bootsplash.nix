571e750ab9173257e73bb35fdc2dffc6b83c0cda
Matt Sturgeon <matt@sturgeon.me.uk>
2025-08-30 16:29:14 +0100

    fix(bootsplash): backport fix for `boot.plymouth.themePackages`
    Backports the fix from https://github.com/NixOS/nixpkgs/pull/438418
    
    Co-authored-by: Sandro Jäckel <sandro.jaeckel@gmail.com>

--- modules/nixos/bootsplash.nix
+++ modules/nixos/bootsplash.nix
@@ -1,11 +1,21 @@
 {
   lib,
   config,
+  options,
+  pkgs,
   ...
 }:
 let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.custom.boot;
+
+  # Backported from https://github.com/NixOS/nixpkgs/pull/438418
+  nixosBreezePlymouth = pkgs.kdePackages.breeze-plymouth.override {
+    logoFile = config.boot.plymouth.logo;
+    logoName = "nixos";
+    osName = "NixOS";
+    osVersion = config.system.nixos.release;
+  };
 in
 {
   options.custom.boot = {
@@ -17,6 +27,10 @@ in
       plymouth = {
         enable = true;
         theme = "breeze";
+        themePackages =
+          lib.throwIf (options.boot.plymouth.themePackages.default != [ ])
+            "${toString ./bootsplash.nix}: `${options.boot.plymouth.themePackaegs}` has been fixed, the manual backport can be removed"
+            [ nixosBreezePlymouth ];
       };
       kernelParams = [ "quiet" ];
       initrd.systemd.enable = true;
