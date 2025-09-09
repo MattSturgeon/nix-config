c915732ffea095cd9c39b9e9d25086c2a81ac2b3
Matt Sturgeon <matt@sturgeon.me.uk>
2025-09-09 19:20:07 +0100

    feat(intellij): wrap with libraries
    Previously I defined `LD_LIBRARY_PATH` globally, however that isn't
    ideal.
    
    Instead, implement a wrapper for intellij to define `LD_LIBRARY_PATH`
    just for it.

--- modules/home-manager/intellij.nix
+++ modules/home-manager/intellij.nix
@@ -12,26 +12,66 @@ let
   ];
 
   cfg = config.custom.editors;
+
+  idea = with pkgs.jetbrains; plugins.addPlugins idea-community-bin idea-plugins;
+
+  # NOTE: the jetbrains packages already do similar wrapping internally.
+  # TODO: make it easier to extend `extraLdPath` and other arguments via overrides,
+  # e.g. by using a finalAttrs-style derivation.
+  ideaWrapped =
+    pkgs.runCommand idea.name
+      {
+        env.ide = idea;
+        env.roodDir = idea.meta.mainProgram;
+
+        nativeBuildInputs = with pkgs; [
+          makeWrapper
+        ];
+
+        inherit (idea) meta passthru;
+      }
+      ''
+        cp -r "$ide" "$out"
+        chmod +w -R "$out"
+
+        (
+          shopt -s nullglob
+
+          for exe in "$out/$rootDir"/bin/*
+          do
+            if [ -x "$exe" ] && ( file "$exe" | grep -q 'text' )
+            then
+              substituteInPlace "$exe" --replace-quiet "$ide" "$out"
+            fi
+            wrapProgram "$exe" \
+              --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath cfg.extraIdeaLibs}
+          done
+        )
+      '';
 in
 {
   options.custom.editors = {
     idea = mkEnableOption "Enable Intellij IDEA";
+    extraIdeaLibs = lib.mkOption {
+      type = with lib.types; listOf package;
+      default = [ ];
+      description = "Extra packages to include on idea's `LD_LIBRARY_PATH`.";
+    };
   };
 
-  config = mkIf cfg.idea {
-    home.packages = with pkgs.jetbrains; [
-      (plugins.addPlugins idea-community-bin idea-plugins)
+  config = {
+    home.packages = mkIf cfg.idea [
+      ideaWrapped
     ];
 
-    home.sessionVariables = {
-      # Needed to launch Minecraft in Intellij
-      LD_LIBRARY_PATH = lib.makeLibraryPath (
-        with pkgs;
-        [
-          libglvnd
-          pulseaudio
-        ]
-      );
-    };
+    # Needed to launch Minecraft in Intellij
+    custom.editors.extraIdeaLibs = with pkgs; [
+      flite
+      glfw-wayland-minecraft
+      libGL
+      libpulseaudio
+      libusb1
+      udev
+    ];
   };
 }
