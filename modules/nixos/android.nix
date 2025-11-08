bd557f3eca75f9df76a4f73caa2b81c29114f886
Matt Sturgeon <matt@sturgeon.me.uk>
2025-11-08 11:29:48 +0000

    chore(android): `android-udev-rules` has been removed
    `android-udev-rules` has been removed due to being superseded by
    built-in systemd uaccess rules.

--- modules/nixos/android.nix
+++ modules/nixos/android.nix
@@ -1,11 +1,6 @@
-{ pkgs, ... }:
 {
   config = {
-
     users.users.matt.extraGroups = [ "kvm" ];
-
-    services.udev.packages = [
-      pkgs.android-udev-rules
-    ];
+    programs.adb.enable = true;
   };
 }
