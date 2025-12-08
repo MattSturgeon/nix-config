b3a4b077cc1a7a02c5e1c5753c00d38353459fd3
Matt Sturgeon <matt@sturgeon.me.uk>
2025-12-08 10:33:20 +0000

    feat(emacs): fetch GTK darkmode patch
    Backport a patch from emacs 31.1 that makes emacs PGTK respect dark and
    light desktop modes.
    
    https://enzuru.medium.com/pure-gtk-emacs-will-now-respect-dark-mode-toggling-for-drawing-the-title-bar-and-widgets-ad78651329fb

--- modules/home-manager/emacs.nix
+++ modules/home-manager/emacs.nix
@@ -17,7 +17,16 @@ in
   config = lib.mkIf cfg.emacs {
     programs.emacs = {
       enable = true;
-      package = pkgs.emacs30-pgtk;
+      package = pkgs.emacs-pgtk.overrideAttrs (prevAttrs: {
+        patches = prevAttrs.patches or [ ] ++ [
+          (pkgs.fetchpatch {
+            name = "Automatically-toggle-between-dark-and-light-mode-PGTK-widgets.patch";
+            url = "https://cgit.git.savannah.gnu.org/cgit/emacs.git/patch/?id=dd9d1df4fa6c96acecaae94461d962da1d822494";
+            hash = "sha256-Z4PfdPlEXm0Q3VltyUT2BOxmhXM3nUTbstfuwFvmuhU=";
+            excludes = [ "etc/NEWS" ];
+          })
+        ];
+      });
 
       extraPackages =
         epkgs: with epkgs; [
