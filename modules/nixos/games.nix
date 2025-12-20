dee5c2e12cb73d4be7020676be9dc7c91c083506
Matt Sturgeon <matt@sturgeon.me.uk>
2025-12-20 22:32:36 +0000

    fix(fmt): reformat modules/nixos/games.nix

--- modules/nixos/games.nix
+++ modules/nixos/games.nix
@@ -28,15 +28,17 @@ in
       prismlauncher
       steam-run
       (inputs.umu-launcher.packages.${system}.default.override (prev: {
-        umu-launcher-unwrapped = prev.umu-launcher-unwrapped.overrideAttrs (finalAttrs: prevAttrs: {
-          # FIXME: Disable tests for now...
-          # - Many tests are failing: https://github.com/Open-Wine-Components/umu-launcher/pull/574
-          # - `versionCheckHook` is failing: https://github.com/Open-Wine-Components/umu-launcher/pull/575
-          doInstallCheck =
-            lib.throwIf (lib.hasInfix "-unstable-" finalAttrs.version)
-              "Disabling umu versionCheckHook is not needed anymore"
-              false;
-        });
+        umu-launcher-unwrapped = prev.umu-launcher-unwrapped.overrideAttrs (
+          finalAttrs: prevAttrs: {
+            # FIXME: Disable tests for now...
+            # - Many tests are failing: https://github.com/Open-Wine-Components/umu-launcher/pull/574
+            # - `versionCheckHook` is failing: https://github.com/Open-Wine-Components/umu-launcher/pull/575
+            doInstallCheck =
+              lib.throwIf (lib.hasInfix "-unstable-" finalAttrs.version)
+                "Disabling umu versionCheckHook is not needed anymore"
+                false;
+          }
+        );
       }))
       mcpelauncher-ui-qt
       nexusmods-app-unfree
