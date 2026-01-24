892dd812acf746b6c33b4529a10ecf96c3f154c3
Matt Sturgeon <matt@sturgeon.me.uk>
2026-01-24 20:32:38 +0000

    feat(minecraft): initial modrinth lockfile
    Adds a JSON lockfile to map Modrinth version IDs to the version's
    download URL and sha512 hash.
    
    Update hosts/desktop's 'quad' server to use the minecraft-modrinth-lock
    module's `mods` option, which loads from the new lockfile.
    
    This enables a future commit to generate the lockfile, meaning only
    version IDs must be manually maintained.

--- hosts/desktop/minecraft.nix
+++ hosts/desktop/minecraft.nix
@@ -1,15 +1,24 @@
-{ inputs, pkgs, ... }:
+{
+  self,
+  inputs,
+  pkgs,
+  ...
+}:
 let
   inherit (pkgs.stdenv.hostPlatform) system;
   inherit (inputs.nix-minecraft.legacyPackages.${system}) minecraftServers;
 in
 {
-  imports = [ inputs.nix-minecraft.nixosModules.minecraft-servers ];
+  imports = [
+    self.nixosModules.minecraft-modrinth-lock
+    inputs.nix-minecraft.nixosModules.minecraft-servers
+  ];
 
-  services.minecraft-servers = {
+  config.services.minecraft-servers = {
     enable = true;
     eula = true;
     openFirewall = true;
+    locks.modrinth = ../../modrinth.lock;
     servers = {
       "quad" = {
         enable = true;
@@ -26,33 +35,16 @@ in
           white-list = false;
           server-port = 43000;
         };
-        # Add or update mods using their modrinth version id:
-        # nix run .#modrinth-prefetch -- <version_id>
-        symlinks.mods = pkgs.linkFarm "mods" {
-          "fabric-api.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/gB6TkYEJ/fabric-api-0.140.2%2B1.21.11.jar";
-            sha512 = "af4465797d80401021a6aefc8c547200e7c0f8cae134299bf3fafbc310fa81f055246b0614fc0e037538f4a844e55aad30abfa3c67460422853dfc426f086d00";
-          };
-          "lithium.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/4DdLmtyz/lithium-fabric-0.21.1%2Bmc1.21.11.jar";
-            sha512 = "0857d30d063dc704a264b2fe774a7e641926193cfdcde72fe2cd603043d8548045b955e30c05b1b2b96ef7d1c0f85d55269da26f44a0644c984b45623e976794";
-          };
-          "ferrite-core.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/uXXizFIs/versions/eRLwt73x/ferritecore-8.0.3-fabric.jar";
-            sha512 = "be600543e499b59286f9409f46497570adc51939ae63eaa12ac29e6778da27d8c7c6cd0b3340d8bcca1cc99ce61779b1a8f52b990f9e4e9a93aa9c6482905231";
-          };
-          "simple-voice-chat.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/9eGKb6K1/versions/T42QJY4i/voicechat-fabric-1.21.11-2.6.10.jar";
-            sha512 = "5da377423b2e48cf6729eab3f67eb82b21d591dee8ff060b2f9295c2743994ca3b49abe77c4b8d2480286c7e6e71f2f19afaf45e13d3794685b726cf2431fc19";
-          };
-          "shulker-box-tooltip.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/2M01OLQq/versions/8Z4OG11C/shulkerboxtooltip-fabric-5.2.14%2B1.21.11.jar";
-            sha512 = "55490c21c98a6c5bd704674a7435a7df2853e9e7ab8872b2a62264444110ad11eac23133e0c1e36ff484531bf849d5fa4a71eb0bf32a9b9c4f3afe33779d1c71";
-          };
-          "apple-skin.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/EsAfCjCV/versions/pvcLnrm0/appleskin-fabric-mc1.21.11-3.0.7.jar";
-            sha512 = "dfc990170b969f3213a9912d13c3fc0d067e2e88faf1a6c7a69bd1a463cd6144ac2dcaeb6a2a3150b595378c1f9449fb0740714ff7703c18c93f8ae3c9eceaa3";
-          };
+
+        # Modrinth mods defined by their version IDs.
+        # To update, lookup the version ID of the mod file and edit `modrinth.lock`.
+        mods = {
+          fabric-api.modrinth = "gB6TkYEJ";
+          lithium.modrinth = "4DdLmtyz";
+          ferrite-core.modrinth = "eRLwt73x";
+          simple-voice-chat.modrinth = "T42QJY4i";
+          shulker-box-tooltip.modrinth = "8Z4OG11C";
+          apple-skin.modrinth = "pvcLnrm0";
         };
       };
     };
