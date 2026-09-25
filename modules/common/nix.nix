8b7cb0def42095a52b0782d9391f0b8ec172f75b
Matt Sturgeon <matt@sturgeon.me.uk>
2026-09-25 15:45:23 +0100

    chore(nix-settings): fix `experimental-features` type
    The module now enforces list of string.

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -29,7 +29,10 @@ in
 
       settings = {
         # Enable flakes and new 'nix' command
-        experimental-features = "nix-command flakes";
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
 
         # Deduplicate and optimize nix store
         auto-optimise-store = true;
